026f3a9b6cea1d38ead17ea20728ff508da8a95b
CherryPicker19 <bashkatov.artm@gmail.com>
2025-06-08 19:10:15 +0300

    Update place_pieces_dialog.py

--- place_pieces_dialog.py
+++ place_pieces_dialog.py
@@ -4,6 +4,11 @@ from board_view import BoardView, PIECE_COLOR, TILE_COLOR, ATTACKED_TILE_COLOR
 
 class PlacePiecesWidget(QDialog):
     def __init__(self, parent):
+        """
+        Initializing window with board, that user can click on.
+        Left Mouse Button - Place Piece, Right Mouse Button - Delete Piece.
+        :param parent: Parent class
+        """
         super().__init__(parent)
         self.setWindowTitle("Place Pieces")
         self.parent = parent
@@ -39,6 +44,12 @@ class PlacePiecesWidget(QDialog):
         self.setLayout(layout_main)
 
     def tile_clicked(self, x, y):
+        """
+        Called upon clicking tile. Adds piece and moves on the board.
+        :param x: X coordinate
+        :param y: Y coordinate
+        :return: None
+        """
         board = self.chess.board
         if self.tiles[x][y].is_LMB and (not((x, y) in self.placed_pieces)):
             self.tiles[x][y].setBrush(PIECE_COLOR)
@@ -60,11 +71,15 @@ class PlacePiecesWidget(QDialog):
                     self.tiles_under_attack.remove((i[0], i[1]))
         elif (x, y) in self.tiles_under_attack:
             return None
-
         else:
             return None
 
     def accept_bt_clicked(self):
+        """
+        Called upon user clicking accept button.
+        Changes tile_under_attack, placed_pieces and chess of a parent class.
+        :return: None
+        """
         self.parent.tiles_under_attack = self.tiles_under_attack
         self.parent.placed_pieces = self.placed_pieces
         self.parent.chess = self.chess
